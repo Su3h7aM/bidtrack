b8391cbaa18b63bbc34b414fa15c3334dd78dcf5
Su3h7aM <matheusgasource@gmail.com>
2025-05-28 09:50:02 -0300

    feat(db): get db URL from environment variable

--- src/db/database.py
+++ src/db/database.py
@@ -1,18 +1,16 @@
 from sqlmodel import SQLModel, Session, create_engine
+import os
 
-DATABASE_URL = "sqlite:///data/bidtrack.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+if DATABASE_URL is not None:
+    engine = create_engine(DATABASE_URL)
 
 
 def init_db():
     SQLModel.metadata.create_all(engine)
 
 
-def db_add(table):
+def get_session():
     with Session(engine) as session:
-        session.add(table)
-        session.commit()
-        session.refresh(table)
-
-    return table
+        yield session
